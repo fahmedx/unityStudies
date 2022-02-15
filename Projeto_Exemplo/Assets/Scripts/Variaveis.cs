a007f63e4d9f1b65a3e049a31ba445ef15829d07
fahmedx <fahmesalimdx@gmail.com>
2022-02-15 00:27:18 -0300

    first commit

---
+++ Projeto_Exemplo/Assets/Scripts/Variaveis.cs
@@ -0,0 +1,155 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variaveis : MonoBehaviour
+{
+    //Podemos criar as variaveis aqui fora ("Global")
+    /* int moeda;
+    int i; 
+    int heroiMoedas = 100;
+    int precoVida = 100; 
+    int estados = 1;
+    float bufunfa; // = 10.6f; 
+    bool trufal;
+    string result;
+    */
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Aqui podemos atribuir valores às variáveis ANTES do primeiro frame.
+        
+        /*
+        Algumas conversões:
+
+        Para inteiro:
+        //moeda = (int)bufunfa;
+
+        Para String:
+
+        //text = moeda.ToString();
+
+        Operações:
+        x = y + z (Soma) 
+        x = y - z (Subtração)
+        x = y * z (Multiplicação)
+        x = y / z (Divisão)
+
+        x = y % z (Modulo)
+        x = y ^ z (Potência)
+
+        Teste de print Objeto moeda:
+        moeda = 10;
+        print(text);
+
+
+        Incremento:
+        int i = i++;
+        ou
+        int i +=1;
+
+        Decremento:
+        int i = i--;
+        ou
+        int i -=1;
+
+        Multiplicação:
+
+        int i *=2;
+        
+        Divisão:
+
+        int i /=2;
+
+
+        -- Operadores Relacionais ---
+
+        == igual
+        != Diferente 
+        <  Menor
+        >  Maior
+        <= Menor Igual
+        >= Maior Igual
+        
+
+        int x = 1;
+        int y = 2;
+
+        print (x == y); False
+        print (x != y); True 
+        print (x < y);  True 
+        print (x > y);  False
+        print (x <= y); True
+        print (x >= y); False
+
+        -- Operadores Lógicos --
+        AND
+        &&
+        print (x != y && y != x); True
+
+        OR 
+        ||
+        
+        print (x != y || y != x); True
+
+        NOT
+        !
+        print (!(x == y)); True
+
+
+        --Se e Senão--
+        
+        if (heroiMoedas == precoVida){
+            print("Olá Herói, você pode comprar Vida, mas fica sem um tustão furado...");
+        }else if(heroiMoedas >= precoVida){
+            print("Olá Herói, você pode comprar Vida e lhe sobra dinheiro!");
+        }else{
+            print("Adquira mais moedas!");
+        }
+
+
+        --Operador Ternario--
+
+        result = (heroiMoedas >= precoVida)?"Posso Comprar a Vida":"Adquira mais moedas!";
+
+        print(result);
+
+
+
+        --Switch--
+        Switches em contexto de jogo:
+
+        Estados de um vilão:
+        Estado 1: Corajoso.
+        Estado 2: Medio.
+        Estado 3: Covardia.
+
+        switch(estados){
+            case 1:
+                print("Ataque!");
+                break;
+            case 2:
+                print("Ataque e defenda");
+                break;
+            case 3: 
+                print("Corra!");
+                break;
+            default:
+                print("Estado não disponível");
+                break;
+        }
+
+        */
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Update é um espaço do código que funciona como um for, onde cada interação dentrro do frame = "i=i++;"
+        //moeda += 1;
+        //print(moeda);
+    }
+}
